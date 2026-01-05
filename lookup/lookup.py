0a20b8522c000bec951a1bff6d92b00c3d125987
John Lukach <john@lukach.net>
2026-01-05 12:24:45 +0000

    test

--- lookup/lookup.py
+++ lookup/lookup.py
@@ -53,10 +53,6 @@ def handler(event, context):
     proxy = proxydb.get_all(ip)
     proxydb.close()
 
-    print(asn)
-    print(db)
-    print(proxy)
-
     f = open('PX12LITEBIN.updated', 'r')
     proxyupdated = f.read()
     f.close()
@@ -66,9 +62,63 @@ def handler(event, context):
     code = 200
     msg = {
         'ip':str(ip),
-        'asn': asn,
-        'db': db,
-        'proxy': proxy,
+        'geo': {
+            'country_short':db.country_short,
+            'country_long':db.country_long,
+            'region':db.region,
+            'city':db.city,
+            'latitude':db.latitude,
+            'longitude':db.longitude,
+            'zip':db.zipcode,
+            'timezone':db.timezone
+        },
+        'asn': {
+            'asn':asn.asn, 
+            'as_name': asn.as_name,
+            'as_domain': asn.as_domain,
+            'as_usagetype': asn.as_usagetype,
+            'as_cidr': asn.as_cidr,
+            'country_short': asn.country_short,
+            'country_long': asn.country_long,
+            'region': asn.region,
+            'city': asn.city,
+            'isp': asn.isp,
+            'latitude': asn.latitude,
+            'longitude': asn.longitude,
+            'domain': asn.domain,
+            'zipcode': asn.zipcode,
+            'timezone': asn.timezone,
+            'netspeed': asn.netspeed,
+            'idd_code': asn.idd_code,
+            'area_code': asn.area_code,
+            'weather_code': asn.weather_code,
+            'weather_name': asn.weather_name,
+            'mcc': asn.mcc,
+            'mnc': asn.mnc,
+            'mobile_brand': asn.mobile_brand, 
+            'elevation': asn.elevation,
+            'usage_type': asn.usage_type,
+            'address_type': asn.address_type,
+            'category': asn.category,
+            'district': asn.district
+        },
+        'proxy': {
+            'is_proxy': proxy['is_proxy'],
+            'proxy_type': proxy['proxy_type'],
+            'country_short': proxy['country_short'],
+            'country_long': proxy['country_long'],
+            'region': proxy['region'],
+            'city': proxy['city'],
+            'isp': proxy['isp'],
+            'domain': proxy['domain'],
+            'usage_type': proxy['usage_type'],
+            'asn': proxy['asn'],
+            'as_name': proxy['as_name'],
+            'last_seen': proxy['last_seen'],
+            'threat': proxy['threat'],
+            'provider': proxy['provider'],
+            'fraud_score': proxy['fraud_score']
+        },
         'attribution':desc,
         'IP2LOCATION-LITE-ASN':asnupdated,
         'IP2LOCATION-LITE-DB11':dbupdated,
