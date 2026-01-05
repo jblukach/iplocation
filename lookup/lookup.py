3b8c30c796f5bac94f24c9765b79b9ac1eeb1b60
John Lukach <john@lukach.net>
2026-01-05 01:01:53 +0000

    test

--- lookup/lookup.py
+++ lookup/lookup.py
@@ -1,11 +1,76 @@
+import ip2location
+import ipaddress
 import json
 import os
 
 def handler(event, context):
 
-    os.system('ls -lh /var/task')
+    try:
+
+        try:
+            ip = ipaddress.ip_address(event['rawQueryString'])
+            ip = str(event['rawQueryString'])
+        except ValueError:
+            ip = ipaddress.ip_address(event['requestContext']['http']['sourceIp'])
+            ip = str(event['requestContext']['http']['sourceIp'])
+
+        asnipv4db = ip2location.ip2location('IP2LOCATION-LITE-ASN.BIN')
+        asnipv6db = ip2location.ip2location('IP2LOCATION-LITE-ASN.IPV6.BIN')
+        cityipv4db = ip2location.ip2location('IP2LOCATION-LITE-DB11.BIN')
+        cityipv6db = ip2location.ip2location('IP2LOCATION-LITE-DB11.IPV6.BIN')
+        proxydb = ip2location.ip2location('IP2PROXY-LITE-PX12.BIN')
+        
+        asnipv4 = asnipv4db.get_all(ip)
+        asnipv6 = asnipv6db.get_all(ip)
+        cityipv4 = cityipv4db.get_all(ip)
+        cityipv6 = cityipv6db.get_all(ip)
+        proxyaddr = proxydb.get_all(ip)
+
+        f = open('DBASNLITEBIN.updated', 'r')
+        asnipv4updated = f.read()
+        f.close()
+
+        f = open('DBASNLITEBINIPV6.updated', 'r')
+        asnipv6updated = f.read()
+        f.close()
+
+        f = open('DB11LITEBIN.updated', 'r')
+        cityipv4updated = f.read()
+        f.close()
+
+        f = open('DB11LITEBINIPV6.updated', 'r')
+        cityipv6updated = f.read()
+        f.close()
+
+        f = open('PX12LITEBIN.updated', 'r')
+        proxyupdated = f.read()
+        f.close()
+
+        desc = 'The lukach.io API uses the IP2Location LITE database for IP geolocation from https://lite.ip2location.com.'
+
+        code = 200
+        msg = {
+            'ip':str(ip),
+            'asn ipv4': asnipv4,
+            'asn ipv6': asnipv6,
+            'city ipv4': cityipv4,
+            'city ipv6': cityipv6,
+            'proxy': proxyaddr,
+            'attribution':desc,
+            'IP2LOCATION-LITE-ASN.BIN':asnipv4updated,
+            'IP2LOCATION-LITE-ASN.IPV6.BIN':asnipv6updated,
+            'IP2LOCATION-LITE-DB11.BIN':cityipv4updated,
+            'IP2LOCATION-LITE-DB11.IPV6.BIN':cityipv6updated,
+            'IP2PROXY-LITE-PX12.BIN':proxyupdated,
+            'region': os.environ['AWS_REGION']
+        }
+
+    except:
+        code = 404
+        msg = 'Invalid IP Address'
+        pass
 
     return {
-        'statusCode': 200,
-        'body': json.dumps('Completed!')
+        'statusCode': code,
+        'body': json.dumps(msg, indent = 4)
     }
